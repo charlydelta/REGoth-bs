b45ae0ecd0e3fc5e81ce816ce86402ecfdb63793
charlydelta <git@charlydelta.de>
2019-07-12 16:16:55 +0200

    Feature: Employ cxxopts

--- src/main_WorldViewer.cpp
+++ src/main_WorldViewer.cpp
@@ -1,4 +1,5 @@
 #include "BsFPSCamera.h"
+#include <cxxopts.hpp>
 #include <REGothEngine.hpp>
 #include <Components/BsCCamera.h>
 #include <Scene/BsPrefab.h>
@@ -44,7 +45,7 @@ public:
   {
     using namespace REGoth;
 
-    const bs::String WORLD    = "WORLD.ZEN";
+    const bs::String WORLD    = "NEWWORLD.ZEN";
     const bs::String SAVEGAME = "WorldViewer-" + WORLD;
 
     bs::HPrefab worldPrefab = GameWorld::load(SAVEGAME);
@@ -91,6 +92,13 @@ protected:
 
 int main(int argc, char** argv)
 {
+  cxxopts::Options options("REGothWorldViewer", "Viewer for Gothic and Gothic 2 worlds");
+  options.add_options()
+    ("h,help", "Print this help message")
+    ("v,version", "Print the REGoth version")
+    ("w,world", "World name to load", cxxopts::value<std::string>())
+    ;
+
   REGothWorldViewer regoth;
 
   return REGoth::main(regoth, argc, argv);
